c4b23e1dbdc1a599d6a4b9205155f1157c178d56
leifarne <leif.arne.rones@gmail.com>
2017-11-01 22:00:32 +0000

    Formatting

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ function dutt() {
 
 var url = "https://formue.azurewebsites.net/api/HttpTriggerJS1?code=AXeaHTx6U6y6GjekOgGjZI0GJ0w7Al3HaYeojJaooVwQt85rrSQXTA==&name=yy";
 
-var stocks = 100;
+var stocks = 7712;
 
 function loadDoc() {
     var xhttp = new XMLHttpRequest();
@@ -17,16 +17,20 @@ function loadDoc() {
             var o = JSON.parse(this.responseText);
             console.log(o.ACN.regularMarketPrice, o.ACN.regularMarketChangePercent, o.ACN.regularMarketPrice, o.ACN.regularMarketChangePercent);
 
+            var numFmt = new Intl.NumberFormat("no-NO", { style: "decimal", minimumFractionDigits: 2, maximumFractionDigits: 2 });
+            var bignumFmt = new Intl.NumberFormat("no-NO", { style: "decimal", maximumFractionDigits: 0 });
+            var pctFmt = new Intl.NumberFormat("no-NO", { style: "percent", minimumFractionDigits: 2, maximumFractionDigits: 2 });
+            
             var totalPrice = o.ACN.regularMarketPrice * o.NOK.regularMarketPrice * stocks;
             var previousTotalPrice = o.ACN.regularMarketPreviousClose * o.NOK.regularMarketPreviousClose * stocks;
             var totalChange = (totalPrice - previousTotalPrice) / previousTotalPrice;
             
-            document.getElementById("acn-price").innerHTML = o.ACN.regularMarketPrice.toFixed(2);
-            document.getElementById("acn-change").innerHTML = (o.ACN.regularMarketChangePercent * 100).toFixed(2) + '%';
-            document.getElementById("usd-price").innerHTML = o.NOK.regularMarketPrice.toFixed(2);
-            document.getElementById("usd-change").innerHTML = (o.NOK.regularMarketChangePercent * 100).toFixed(2) + '%';
-            document.getElementById("total-price").innerHTML = totalPrice.toLocaleString(); // toFixed(0);
-            document.getElementById("total-change").innerHTML = (totalChange * 100).toFixed(2) + '%';
+            document.getElementById("acn-price").innerHTML = numFmt.format(o.ACN.regularMarketPrice);
+            document.getElementById("acn-change").innerHTML = pctFmt.format(o.ACN.regularMarketChangePercent);
+            document.getElementById("usd-price").innerHTML = numFmt.format(o.NOK.regularMarketPrice);
+            document.getElementById("usd-change").innerHTML = pctFmt.format(o.NOK.regularMarketChangePercent);
+            document.getElementById("total-price").innerHTML = bignumFmt.format(totalPrice);
+            document.getElementById("total-change").innerHTML = pctFmt.format(totalChange);
 
             if (o.ACN.regularMarketChangePercent < 0) {
                 document.getElementById("acn-change").style.color = "red";
@@ -38,8 +42,12 @@ function loadDoc() {
                 document.getElementById("total-change").style.color = "red";
             }
             
-            document.getElementById("demo").innerHTML = this.responseText;
-       }
+            document.getElementById("demo").innerHTML = "Status DONE.";
+        }
+        else {
+            document.getElementById("demo").innerHTML = "Ready state: " + this.readyState;
+        
+        }
     };
     
     xhttp.open("GET", url, true);
